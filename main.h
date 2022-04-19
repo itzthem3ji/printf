d10fe16036374e608fd668ca18c2f69e6be3b5af
Feyitimi Victor <victorfeyitimi@gmail.com>
2022-04-20 00:15:59 +0100

    fxns handle non-custom conversion specifiers

--- main.h
+++ main.h
@@ -5,6 +5,67 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+#define BUFSIZE 1024
+#define SPECIFIERS_SIZE 14
+#define LENGTH_SPECS_SIZE 8
+#define FLAGS_SPECS_SIZE 6
+
+#define ERROR -2
+#define INVALID -1
+#define EMPTY 0
+#define OK 1
+
+/**
+ * struct spec_data - specifiers data
+ *
+ * @spec_flags: - +   0 ' #
+ * @spec_width: minimum number of characters to output
+ * @spec_prec: maximum limit on the output, depending on the type
+ * @spec_length: hh h l ll L z j t
+ * @fmt_spec: % d,i u f,F e,E g,G x,X o s,S c p a,A n b r,R
+ * @fmt_len: length of the format specifier
+ * @status: return value of extract functions
+ */
+
+typedef struct spec_data
+{
+	char fmt_spec;
+	char *spec_length;
+	int spec_prec;
+	int spec_width;
+	char *spec_flags;
+	int fmt_len;
+	int status;
+} spec_data_t;
+
+/**
+ * struct pf_buf - _printf buffer
+ *
+ * @buf: the buffer
+ * @len: the size of the string to add to main buffer
+ * @index: index of last char in main buffer
+ */
+
+typedef struct pf_buf
+{
+	char *buf;
+	int len;
+	int index;
+} pf_buf_t;
+
+/**
+ * struct specs - specifiers structure containing associated funcions
+ *
+ * @spec: the specifier
+ * @func: the print function associated to the specifier
+ */
+
+typedef struct specs
+{
+	char spec;
+	pf_buf_t *(*func)(va_list, spec_data_t*);
+} specs_t;
+
 /**
  * struct print - struct for printer functions
  * @type_arg: identifier
@@ -57,5 +118,15 @@ char *fill_oct_array(char *bnr, char *oct);
 char *fill_long_oct_array(char *bnr, char *oct);
 char *fill_short_oct_array(char *bnr, char *oct);
 char *fill_hex_array(char *bnr, char *hex, int isupp, int limit);
+char *extract_format(const char *format, int size);
+char *extract_length(char *str, spec_data_t *data);
+int extract_prec(char *str, spec_data_t *data);
+int extract_width(char *str, spec_data_t *data);
+char *extract_flags(char *str, spec_data_t *data);
+void *spec_data_t_delete(spec_data_t *data);
+spec_data_t *spec_data_t_new(void);
+int spec_data_t_parse(spec_data_t *data, const char *format);
+int spec_data_t_leave(char *str, int status);
+int is_in_format_specifiers(char c)
 
 #endif
