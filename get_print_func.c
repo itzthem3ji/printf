0422cbcf51c78675d79a6e6ee6890db9a8ebb800
Feyitimi Victor <victorfeyitimi@gmail.com>
2022-04-20 04:46:21 +0100

    new edits

--- get_print_func.c
+++ get_print_func.c
@@ -56,3 +56,102 @@ int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int)
 	}
 	return (pr[i].f);
 }
+
+/**
+ * get_print_func - finds the format func
+ * @s: the format string
+ * @ap: argument pointer
+ * @params: the parameters struct
+ *
+ * Return: the number of bytes printed
+ */
+int get_print_func(char *s, va_list ap, params_t *params)
+{
+	int (*f)(va_list, params_t *) = get_specifier(s);
+
+	if (f)
+		return (f(ap, params));
+	return (0);
+}
+
+/**
+ * get_flag - finds the flag func
+ * @s: the format string
+ * @params: the parameters struct
+ *
+ * Return: if flag was valid
+ */
+int get_flag(char *s, params_t *params)
+{
+	int i = 0;
+
+	switch (*s)
+	{
+		case '+':
+			i = params->plus_flag = 1;
+			break;
+		case ' ':
+			i = params->space_flag = 1;
+			break;
+		case '#':
+			i = params->hashtag_flag = 1;
+			break;
+		case '-':
+			i = params->minus_flag = 1;
+			break;
+		case '0':
+			i = params->zero_flag = 1;
+			break;
+	}
+	return (i);
+}
+
+/**
+ * get_modifier - finds the modifier func
+ * @s: the format string
+ * @params: the parameters struct
+ *
+ * Return: if modifier was valid
+ */
+int get_modifier(char *s, params_t *params)
+{
+	int i = 0;
+
+	switch (*s)
+	{
+	case 'h':
+		i = params->h_modifier = 1;
+		break;
+	case 'l':
+		i = params->l_modifier = 1;
+		break;
+	}
+	return (i);
+}
+
+/**
+ * get_width - gets the width from the format string
+ * @s: the format string
+ * @params: the parameters struct
+ * @ap: the argument pointer
+ *
+ * Return: new pointer
+ */
+char *get_width(char *s, params_t *params, va_list ap)
+/* should this function use char **s and modify the pointer? */
+{
+	int d = 0;
+
+	if (*s == '*')
+	{
+		d = va_arg(ap, int);
+		s++;
+	}
+	else
+	{
+		while (_isdigit(*s))
+			d = d * 10 + (*s++ - '0');
+	}
+	params->width = d;
+	return (s);
+}
